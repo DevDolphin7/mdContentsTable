122c8f5cb1ebcc1ddbd7f5f8b180613496ec1468
Dan Olver <devdolphin7@outlook.com>
2024-06-20 17:05:03 +0100

    Black formatter python formatter extension installed - code re-formatted

--- __tests__/md_contents_table_recursion.py
+++ __tests__/md_contents_table_recursion.py
@@ -25,100 +25,104 @@ Text
 import md_contents_table as mdCT
 import pytest
 
+
 def describe_initialisation():
-  def test_raises_appropriate_errors():
-    # Initialising with a file path that does not end with ".md" raises a ValueError
-    with pytest.raises(ValueError):
-      mdCT.md_contents_table("./test 1 read file")
+    def test_raises_appropriate_errors():
+        # Initialising with a file path that does not end with ".md" raises a ValueError
+        with pytest.raises(ValueError):
+            mdCT.md_contents_table("./test 1 read file")
 
-    # Initialising with a file that does not exist raises a ValueError
-    with pytest.raises(ValueError):
-      mdCT.md_contents_table("./hello_world.md")
+        # Initialising with a file that does not exist raises a ValueError
+        with pytest.raises(ValueError):
+            mdCT.md_contents_table("./hello_world.md")
 
-  def test_parameters():
-    # Initialising stores the file path as a class variable
-    test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
-    assert test_mdCT.file_path == "./test 1 read file.md"
+    def test_parameters():
+        # Initialising stores the file path as a class variable
+        test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
+        assert test_mdCT.file_path == "./test 1 read file.md"
 
-    # Initialising stores an optional bool that removes a previously generated contents table
-    test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
-    assert test_mdCT.remove_current_table == True
+        # Initialising stores an optional bool that removes a previously generated contents table
+        test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
+        assert test_mdCT.remove_current_table == True
 
-    test_mdCT = mdCT.md_contents_table("./test 1 read file.md", False)
-    assert test_mdCT.remove_current_table == False
+        test_mdCT = mdCT.md_contents_table("./test 1 read file.md", False)
+        assert test_mdCT.remove_current_table == False
 
-  def test_memory_reset():
-    # Initialising resets all object properties that are not parameters
-    test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
-    assert test_mdCT._file_contents == None
-    assert test_mdCT._headings == None
+    def test_memory_reset():
+        # Initialising resets all object properties that are not parameters
+        test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
+        assert test_mdCT._file_contents == None
+        assert test_mdCT._headings == None
 
 
 # Read and store the contents of md file
 def describe_reading_and_storing_file_contents():
-  def test_input_file_is_read():
-    # The contents of a simple file is read and stored
-    test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
-    test_mdCT.read_file_contents()
-    assert test_mdCT._file_contents == "Hello, world!"
+    def test_input_file_is_read():
+        # The contents of a simple file is read and stored
+        test_mdCT = mdCT.md_contents_table("./test 1 read file.md")
+        test_mdCT.read_file_contents()
+        assert test_mdCT._file_contents == "Hello, world!"
 
-  def test_input_file_is_not_mutated():
-    # The file is not mutated (uses os.path.getmtime <- get time file was last modified)
-    input_file_path = "./test 1 read file.md"
-    with open(input_file_path, "r") as file:
-      before = file.read()
-      file.close()
+    def test_input_file_is_not_mutated():
+        # The file is not mutated (uses os.path.getmtime <- get time file was last modified)
+        input_file_path = "./test 1 read file.md"
+        with open(input_file_path, "r") as file:
+            before = file.read()
+            file.close()
 
-    test_mdCT = mdCT.md_contents_table(input_file_path)
-    test_mdCT.read_file_contents()
+        test_mdCT = mdCT.md_contents_table(input_file_path)
+        test_mdCT.read_file_contents()
 
-    with open(input_file_path, "r") as file:
-      mutated = file.read()
-      file.close()
+        with open(input_file_path, "r") as file:
+            mutated = file.read()
+            file.close()
 
-    assert (before == mutated) == True
+        assert (before == mutated) == True
 
 
 # Find and store all the headings from the md file contents
 ## Numbered according to the number of #
 def describe_find_and_store_headings():
-  def test_able_to_store_headings():
-    ## Stores a list of the headings found
-    test_mdCT = mdCT.md_contents_table("./test 2 no headings.md")
-    test_mdCT.read_file_contents()
-    test_mdCT.find_headings()
-    assert test_mdCT._headings == []
+    def test_able_to_store_headings():
+        ## Stores a list of the headings found
+        test_mdCT = mdCT.md_contents_table("./test 2 no headings.md")
+        test_mdCT.read_file_contents()
+        test_mdCT.find_headings()
+        assert test_mdCT._headings == []
 
-  def test_finds_one_heading():
-    ## Finds 1 top level heading
-    test_mdCT = mdCT.md_contents_table("./test 3 1 top level heading.md")
-    test_mdCT.read_file_contents()
-    test_mdCT.find_headings()
-    assert test_mdCT._headings == ["# Hello"]
+    def test_finds_one_heading():
+        ## Finds 1 top level heading
+        test_mdCT = mdCT.md_contents_table("./test 3 1 top level heading.md")
+        test_mdCT.read_file_contents()
+        test_mdCT.find_headings()
+        assert test_mdCT._headings == ["# Hello"]
 
-    ## Finds 1 of each level
-    test_mdCT = mdCT.md_contents_table("./test 4 1 each level heading.md")
-    test_mdCT.read_file_contents()
-    test_mdCT.find_headings()
-    assert test_mdCT._headings == ["# Hello",
-                                  "## World",
-                                  "###### Six",
-                                  "### Three",
-                                  "#### Four", 
-                                  "##### Five"]
-    
-  def test_finds_multiple_headings():
-    ## Finds multiple top level headings
-    test_mdCT = mdCT.md_contents_table("./test 5 multiple top level headings.md")
-    test_mdCT.read_file_contents()
-    test_mdCT.find_headings()
-    assert test_mdCT._headings == ["# Hello", "# World"]
+        ## Finds 1 of each level
+        test_mdCT = mdCT.md_contents_table("./test 4 1 each level heading.md")
+        test_mdCT.read_file_contents()
+        test_mdCT.find_headings()
+        assert test_mdCT._headings == [
+            "# Hello",
+            "## World",
+            "###### Six",
+            "### Three",
+            "#### Four",
+            "##### Five",
+        ]
 
-    ## Finds multiple of each heading
+    def test_finds_multiple_headings():
+        ## Finds multiple top level headings
+        test_mdCT = mdCT.md_contents_table("./test 5 multiple top level headings.md")
+        test_mdCT.read_file_contents()
+        test_mdCT.find_headings()
+        assert test_mdCT._headings == ["# Hello", "# World", "# How Are You?"]
 
-    ## Doesn't confuse 1 tag
+        ## Finds multiple of each heading
 
-    ## Doesn't confuse multiple tags
+        ## Doesn't confuse 1 tag
+
+        ## Doesn't confuse multiple tags
+
 
 # Format the contents table ready to be written
 
--- md_contents_table.py
+++ md_contents_table.py
@@ -1,44 +1,51 @@
 import os.path
 
-class md_contents_table():
 
-    def __init__(self, file_path, remove_current_table = True):
+class md_contents_table:
+
+    def __init__(self, file_path, remove_current_table=True):
         """Checks a valid .md file exists in order to create a contents table using create_contents_table().
         Parameters:
         - file_path (str): "path/to/file.md"
-        - remove_current_table (bool, optional): Removes previously made content table if it exists."""
+        - remove_current_table (bool, optional): Removes previously made content table if it exists.
+        """
 
         self._file_contents = None
         self._headings = None
 
         if file_path[-3:] != ".md":
-            raise ValueError(f"Please provide a file path ending in '.md', not: {file_path}")
-        
+            raise ValueError(
+                f"Please provide a file path ending in '.md', not: {file_path}"
+            )
+
         elif not os.path.exists(file_path):
-            raise ValueError(f"Please provide the path to an existing .md file, not: {file_path}")
+            raise ValueError(
+                f"Please provide the path to an existing .md file, not: {file_path}"
+            )
 
         self.file_path = file_path
         self.remove_current_table = remove_current_table
-
 
     def read_file_contents(self):
         with open(self.file_path, "r") as file:
             self._file_contents = file.read()
 
-
     def find_headings(self, contents=None):
         # Initialise recursion
-        if self._headings == None: self._headings = []
-        if contents == None: contents = self._file_contents.split("\n")
+        if self._headings == None:
+            self._headings = []
+        if contents == None:
+            contents = self._file_contents.split("\n")
 
         # Recursive base case
-        if len(contents) == 0: return
+        if len(contents) == 0:
+            return
 
-        if len(contents[0]) != 0: # If line is blank, skip
+        if len(contents[0]) != 0:  # If line is blank, skip
             # If line starts with a "#", add to headings
-            if contents[0][0] == "#": self._headings.append(contents[0])
+            if contents[0][0] == "#":
+                self._headings.append(contents[0])
 
         # Recursive step
         contents.pop(0)
         self.find_headings(contents=contents)
-
