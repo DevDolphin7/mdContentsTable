e7004f1e76f63244be14565eb25ba03a5870363e
Dan Olver <devdolphin7@outlook.com>
2024-06-23 09:55:52 +0100

    Cleaning up directory - test files moved to ./testFiles/

--- __tests__/md_contents_table_recursion.py
+++ __tests__/md_contents_table_recursion.py
@@ -30,20 +30,20 @@ def describe_initialisation():
     def test_raises_appropriate_errors():
         # Initialising with a file path that does not end with ".md" raises a ValueError
         with pytest.raises(ValueError):
-            mdCT.MdContentsTable("./test 1 read file")
+            mdCT.MdContentsTable("./test_files/test 1 read file")
 
         # Initialising with a file that does not exist raises a ValueError
         with pytest.raises(ValueError):
-            mdCT.MdContentsTable("./hello_world.md")
+            mdCT.MdContentsTable("./test_files/hello_world.md")
 
     def test_parameters():
         # Initialising stores the file path as a class variable
-        test_mdCT = mdCT.MdContentsTable("./test 1 read file.md")
-        assert test_mdCT.file_path == "./test 1 read file.md"
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 1 read file.md")
+        assert test_mdCT.file_path == "./test_files/test 1 read file.md"
 
     def test_memory_reset():
         # Initialising resets all object properties that are not parameters
-        test_mdCT = mdCT.MdContentsTable("./test 1 read file.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 1 read file.md")
         assert test_mdCT._file_contents == None
         assert test_mdCT._headings == None
 
@@ -52,13 +52,13 @@ def describe_initialisation():
 def describe__read_file_contents():
     def test_input_file_is_read():
         # The contents of a simple file is read and stored
-        test_mdCT = mdCT.MdContentsTable("./test 1 read file.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 1 read file.md")
         test_mdCT._read_file_contents()
         assert test_mdCT._file_contents == "Hello, world!"
 
     def test_input_file_is_not_mutated():
         # The file is not mutated (uses os.path.getmtime <- get time file was last modified)
-        input_file_path = "./test 1 read file.md"
+        input_file_path = "./test_files/test 1 read file.md"
         with open(input_file_path, "r") as file:
             before = file.read()
             file.close()
@@ -75,7 +75,7 @@ def describe__read_file_contents():
 
 def describe__if_current_table_then_remove():
     def test_removes_existing_contents_table_from_file_contents():
-        test_mdCT = mdCT.MdContentsTable("./test 10 simple output.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 10 simple output.md")
         test_mdCT._read_file_contents()
         test_mdCT._if_current_table_then_remove()
         actual = test_mdCT._file_contents
@@ -86,26 +86,26 @@ def describe__if_current_table_then_remove():
 def describe__find_headings():
     def test_able_to_store_headings():
         ## Creates a list for headings
-        test_mdCT = mdCT.MdContentsTable("./test 2 no headings.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 2 no headings.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert type(test_mdCT._headings) == list
 
         ## Stores a list of the headings found
-        test_mdCT = mdCT.MdContentsTable("./test 2 no headings.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 2 no headings.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert test_mdCT._headings == []
 
     def test_finds_one_heading():
         ## Finds 1 top level heading
-        test_mdCT = mdCT.MdContentsTable("./test 3 1 top level heading.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 3 1 top level heading.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert test_mdCT._headings == ["# Hello"]
 
         ## Finds 1 of each level
-        test_mdCT = mdCT.MdContentsTable("./test 4 1 each level heading.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 4 1 each level heading.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert test_mdCT._headings == [
@@ -119,7 +119,7 @@ def describe__find_headings():
 
     def test_finds_multiple_top_headings():
         ## Finds multiple top level headings
-        test_mdCT = mdCT.MdContentsTable("./test 5 multiple top level headings.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 5 multiple top level headings.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert test_mdCT._headings == ["# Hello", "# World", "# How are you?"]
@@ -127,7 +127,7 @@ def describe__find_headings():
     def test_finds_multiple_each_headings():
         ## Finds multiple of each heading
         test_mdCT = mdCT.MdContentsTable(
-            "./test 6 multiple of each level headings.md"
+            "./test_files/test 6 multiple of each level headings.md"
         )
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
@@ -147,13 +147,13 @@ def describe__find_headings():
 
     def test_doesnt_include_any_tags():
         # Doesn't confuse 1 tag
-        test_mdCT = mdCT.MdContentsTable("./test 7 tag included.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 7 tag included.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert test_mdCT._headings == ["# Hello"]
 
         ## Doesn't confuse multiple tags
-        test_mdCT = mdCT.MdContentsTable("./test 8 complex file.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 8 complex file.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         assert test_mdCT._headings == [
@@ -176,7 +176,7 @@ def describe__find_headings():
 def describe__format_headings():
     def test_able_to_store_formatted_string():
         # Creates a string
-        test_mdCT = mdCT.MdContentsTable("./test 2 no headings.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 2 no headings.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         test_mdCT._format_headings()
@@ -185,7 +185,7 @@ def describe__format_headings():
 
     def test_formats_one_top_level_heading():
         # numbers a top level heading, e.g: 1. <heading>
-        test_mdCT = mdCT.MdContentsTable("./test 3 1 top level heading.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 3 1 top level heading.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         test_mdCT._format_headings()
@@ -194,7 +194,7 @@ def describe__format_headings():
 
     def test_correctly_numbers_each_level():
         # correctly numbers a heading of each level, (e.g 4th level) = 1.1.1.1. <heading>
-        test_mdCT = mdCT.MdContentsTable("./test 4 1 each level heading.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 4 1 each level heading.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         test_mdCT._format_headings()
@@ -209,7 +209,7 @@ def describe__format_headings():
         # 1.2 <heading>
         # 2. <heading>
         # 2.1 <heading> <-- the second level heading was previously at "2."
-        test_mdCT = mdCT.MdContentsTable("./test 9 1 heading reset.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 9 1 heading reset.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         test_mdCT._format_headings()
@@ -220,7 +220,7 @@ def describe__format_headings():
         )
 
     def test_correctly_deals_with_a_complex_file():
-        test_mdCT = mdCT.MdContentsTable("./test 8 complex file.md")
+        test_mdCT = mdCT.MdContentsTable("./test_files/test 8 complex file.md")
         test_mdCT._read_file_contents()
         test_mdCT._find_headings()
         test_mdCT._format_headings()
@@ -234,7 +234,7 @@ def describe__format_headings():
 # Write the contents table then the rest of the file back to the file.
 def describe__write_output():
     def test_adds_a_content_table_to_a_simple_file():
-        input_file_path = "./test 10 simple output.md"
+        input_file_path = "./test_files/test 10 simple output.md"
 
         test_mdCT = mdCT.MdContentsTable(input_file_path)
         test_mdCT._read_file_contents()
@@ -253,7 +253,7 @@ def describe__write_output():
         )
 
     def test_adds_a_content_table_to_a_complex_file():
-        input_file_path = "./test 11 complex file output.md"
+        input_file_path = "./test_files/test 11 complex file output.md"
 
         test_mdCT = mdCT.MdContentsTable(input_file_path)
         test_mdCT._read_file_contents()
@@ -275,7 +275,7 @@ def describe__write_output():
 # Make it convenient for user to interact with
 def describe_CreateContentsTable():
     def test_user_convinient_way_functions_correctly():
-        input_file_path = "./test 12 convenient user interface.md"
+        input_file_path = "./test_files/test 12 convenient user interface.md"
 
         mdCT.CreateContentsTable(input_file_path)
 
@@ -289,7 +289,7 @@ def describe_CreateContentsTable():
         )
     
     # def test_links_to_headings():
-    #     input_file_path = "./test 13 heading links.md"
+    #     input_file_path = "./test_files/test 13 heading links.md"
 
     #     mdCT.CreateContentsTable(input_file_path)
 
@@ -301,3 +301,5 @@ def describe_CreateContentsTable():
     #         actual
     #         == '<a name="start-of-contents" />\n\n# Contents\n\t1. Hello\n\t\t1.1. World\n\t\t\t1.1.1. How\n\t\t\t\t1.1.1.1. Are\n\t\t\t\t\t1.1.1.1.1. You?\n\t\t1.2. I\'m\n\t\t\t\t\t\t1.2.1.1.1.1. Good\n\t2. Thank\n\t\t\t2.1.1. You\n\t\t\t\t2.1.1.1. For\n\t\t\t\t\t2.1.1.1.1. Asking\n<a name="end-of-contents" />\n\n# Hello\n**Hello text!**\n\n## World\nText with a paragraph\n\nAnother paragraph\n\n### How\n```js\nfunction thisIsHow() {\n    console.log("we do it")\n}\n```\n\n#### Are\n> Othr forms of text formatting are available\n##### You?\n\n## I\'m\n#trees\n###### Good\n#seas\n# Thank\nText and then a #tag, why not?\n### You\n`random code snippets` that aren\'t long enough for a big box\n#### For\n##### Asking\n'
     #     )
+
+# Implement heading links idea
